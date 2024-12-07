a5a2821534af515e81c65b3ee4c292466cbc78e8
refat374 <mdarafatkhandakar374@gmail.com>
2024-12-07 21:11:28 +0600

    first commit

---
+++ js/event.js
@@ -0,0 +1,8 @@
+document.getElementById('father').addEventListener('click', function () {
+    console.log('father click');
+
+})
+document.getElementById('item-3').addEventListener('click', function () {
+    console.log('3 no item click');
+
+})
